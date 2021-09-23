2fa448c31961525bf325ea0950c4d521702c18fd
Fabio Nascimento <agsuperpoder@gmail.com>
2021-09-23 16:51:46 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -11,6 +12,10 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
+
+btn2.onclick = function() {
+    modal.style.display = "block";
+  }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
